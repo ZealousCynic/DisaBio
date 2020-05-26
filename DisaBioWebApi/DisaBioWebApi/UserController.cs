e5adc0b8caedf165853ecfb1d5925daf8a7316d4
ZealousCynic <ZealousCynic@gmail.com>
2020-05-26 12:45:50 +0200

    Project initialization... Again

---
+++ DisaBioWebApi/DisaBioWebApi/UserController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DisaBioWebApi
+{
+    public class UserController
+    {
+        public void DoStuff()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
