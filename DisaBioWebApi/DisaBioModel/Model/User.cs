709efee1b84f92c1a00cb4d8c431b04dff557056
ZealousCynic <ZealousCynic@gmail.com>
2020-05-28 09:46:03 +0200

    UserRepository, get and create preliminary

--- DisaBioWebApi/DisaBioModel/Model/User.cs
+++ DisaBioWebApi/DisaBioModel/Model/User.cs
@@ -12,6 +12,7 @@ namespace DisaBioModel.Model
         private string email;
         private string phoneNumber;
         private string password;
+        private int roleID;
         private byte[] salt;
 
         // Properties
@@ -20,17 +21,19 @@ namespace DisaBioModel.Model
         public string Email { get => email; set => email = value; }
         public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
         public string Password { get => password; set => password = value; }
+        public int RoleID { get => roleID; set => roleID = value; }
         public byte[] Salt { get => salt; set => salt = value; }
 
         // Constructor
         public User() : base() { }
-        public User(int id, string firstname, string lastname, string email, string phoneNumber, string password, byte[] salt) : base(id)
+        public User(int id, string firstname, string lastname, string email, string phoneNumber, string password, int roleID, byte[] salt) : base(id)
         {
             Firstname = firstname;
             Lastname = lastname;
             Email = email;
             PhoneNumber = phoneNumber;
             Password = password;
+            RoleID = roleID;
             Salt = salt;
         }
     }
--- DisaBioWebApi/DisaBioModel/Repository/UserRepository.cs
+++ DisaBioWebApi/DisaBioModel/Repository/UserRepository.cs
@@ -7,9 +7,27 @@ namespace DisaBioModel.Repository
 {
     class UserRepository : Interface.IUserRepository<User>
     {
-        public bool Create(User t)
+        public bool Create(User u)
         {
-            throw new NotImplementedException();
+            using(DatabaseConnection dbcon = new DatabaseConnection())
+            {
+                dbcon.Cmd.CommandText = "InsertUser";
+
+                dbcon.Cmd.Parameters.AddWithValue("FirstName", u.Firstname);
+                dbcon.Cmd.Parameters.AddWithValue("LastName", u.Lastname);
+                dbcon.Cmd.Parameters.AddWithValue("Email", u.Email);
+                dbcon.Cmd.Parameters.AddWithValue("PhoneNumber", u.PhoneNumber);
+                dbcon.Cmd.Parameters.AddWithValue("RoleID", u.RoleID);
+                dbcon.Cmd.Parameters.AddWithValue("Password", u.Password);
+                dbcon.Cmd.Parameters.AddWithValue("Salt", u.Salt);
+
+                dbcon.Connect();
+
+                if (dbcon.Cmd.ExecuteNonQuery() == 1)
+                    return true;
+
+            }
+            return false;
         }
 
         public bool Delete(int id)
@@ -24,7 +42,45 @@ namespace DisaBioModel.Repository
 
         public User GetByName(string name)
         {
-            throw new NotImplementedException();
+            User toReturn = null;
+
+            using(DatabaseConnection dbcon = new DatabaseConnection())
+            {
+                dbcon.Cmd.CommandText = "GetByNameUser";
+
+                dbcon.Cmd.Parameters.AddWithValue("Email", name);
+
+                dbcon.Reader = dbcon.Cmd.ExecuteReader();
+
+                if (dbcon.Reader.HasRows)
+                {
+                    string salt = (string)dbcon.Reader.GetValue(6);
+                    byte[] saltBytes = Encoding.ASCII.GetBytes(salt);
+
+                    /*
+                     * 0 = ID
+                     * 1 = FirstName
+                     * 2 = LastName
+                     * 3 = Email
+                     * 4 = PhoneNumber
+                     * 5 = Password
+                     * 6 = RoleID
+                     * saltbytes = salt
+                     */
+                    toReturn = new User(
+                        (int)dbcon.Reader.GetValue(0),
+                        (string)dbcon.Reader.GetValue(1),
+                        (string)dbcon.Reader.GetValue(2),
+                        (string)dbcon.Reader.GetValue(3),
+                        (string)dbcon.Reader.GetValue(4),
+                        (string)dbcon.Reader.GetValue(5),
+                        (int)dbcon.Reader.GetValue(6),
+                        saltBytes
+                        );
+                }
+            }
+
+            return toReturn;
         }
 
         public User[] GetRange(int range)
