995d146b9cb01d6ee417900372206141153fc6d8
Alexander4881 <alihnmikkelsen@gmail.com>
2020-05-28 12:14:37 +0200

    Genre Created and Implimented

--- DisaBioWebApi/DisaBioModel/Repository/GenreRepository.cs
+++ DisaBioWebApi/DisaBioModel/Repository/GenreRepository.cs
@@ -1,6 +1,7 @@
 ﻿using DisaBioModel.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace DisaBioModel.Repository
@@ -9,27 +10,181 @@ namespace DisaBioModel.Repository
     {
         public bool Create(Genre t)
         {
-            throw new NotImplementedException();
+            using (DatabaseConnection conn = new DatabaseConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand("InsertGenre"))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@Name",t.Name);
+
+                    if (cmd.ExecuteNonQuery() == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            using (DatabaseConnection conn = new DatabaseConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand("DeleteGenre"))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@GenreID", id);
+
+                    if (cmd.ExecuteNonQuery() == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool DeleteMovieGenre(int genreID, int movieID)
+        {
+            using (DatabaseConnection conn = new DatabaseConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand("DeleteMovieGenre"))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@GenreID", genreID);
+                    cmd.Parameters.AddWithValue("@movieID", movieID);
+
+                    if (cmd.ExecuteNonQuery() == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
         public Genre GetByID(int id)
         {
-            throw new NotImplementedException();
+            Genre returnGenre = new Genre();
+
+            using (DatabaseConnection conn = new DatabaseConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand("GetGenre"))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@GenreID", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            returnGenre.ID = reader.GetInt32(0);
+                            returnGenre.Name = reader.GetString(1);
+                        }
+                    }
+                }
+            }
+            return returnGenre;
+        }
+
+        public Genre[] GetMovieGenre(int movieID)
+        {
+            List<Genre> genres = new List<Genre>();
+
+            using (DatabaseConnection conn = new DatabaseConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand("GetMovieGenre"))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@MovieID", movieID);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Genre genre = new Genre();
+                            genre.ID = reader.GetInt32(0);
+                            genre.Name = reader.GetString(1);
+
+                            genres.Add(genre);
+                        }
+                    }
+                }
+            }
+            return genres.ToArray();
         }
 
         public Genre[] GetRange(int startRange, int endRange)
         {
-            throw new NotImplementedException();
+            List<Genre> genres = new List<Genre>();
+
+            using (SqlCommand cmd = new SqlCommand("GetRangeGenre"))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@RangeStart", startRange);
+                cmd.Parameters.AddWithValue("@RangeEnd", endRange);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    
+                    while (reader.Read())
+                    {
+                        Genre genre = new Genre();
+                        genre.ID = reader.GetInt32(0);
+                        genre.Name = reader.GetString(1);
+
+                        genres.Add(genre);
+                    }
+                }
+            }
+
+            return genres.ToArray();
+        }
+
+        public bool InsertMovieGenre(int movieID, Genre genre)
+        {
+            using (DatabaseConnection conn = new DatabaseConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand("MovieGenreInsert"))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@MovieID", movieID);
+                    cmd.Parameters.AddWithValue("@GenreName", genre.Name);
+
+                    if (cmd.ExecuteNonQuery() == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
         public bool Update(int id, Genre t)
         {
-            throw new NotImplementedException();
+            using (DatabaseConnection conn = new DatabaseConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand("UpdateGenre"))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@ID", id);
+                    cmd.Parameters.AddWithValue("@Name", t.Name);
+
+                    if (cmd.ExecuteNonQuery() == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
