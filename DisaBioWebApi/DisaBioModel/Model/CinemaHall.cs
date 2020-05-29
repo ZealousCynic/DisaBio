fbd9c12e50225e5628fe4bb621e8c9b7d3ed81cf
Alexander4881 <alihnmikkelsen@gmail.com>
2020-05-29 12:03:18 +0200

    Created Movie and Updated Genre

--- DisaBioWebApi/DisaBioModel/Model/CinemaHall.cs
+++ DisaBioWebApi/DisaBioModel/Model/CinemaHall.cs
@@ -8,25 +8,19 @@ namespace DisaBioModel.Model
     {
         // Attributes
         private int hallNumber;
-        private Seat[,] seats;
-        private MovieShow movieList;
-        private string layoutURL;
+        private List<MovieShow> movieShow;
 
         // Properties
         public int HallNumber { get => hallNumber; set => hallNumber = value; }
-        public MovieShow MovieList { get => movieList; set => movieList = value; }
-        public string LayoutURL { get => layoutURL; set => layoutURL = value; }
-        internal Seat[,] Seats { get => seats; set => seats = value; }
+        public List<MovieShow> MovieShowList { get => movieShow; set => movieShow = value; }
 
         // Constructor
         public CinemaHall():base() { }
 
-        public CinemaHall(int id,int hallNumber, MovieShow movieList, string layoutURL, Seat[,] seats):base(id)
+        public CinemaHall(int id,int hallNumber, List<MovieShow> movieShowList):base(id)
         {
             HallNumber = hallNumber;
-            MovieList = movieList;
-            LayoutURL = layoutURL;
-            Seats = seats;
+            MovieShowList = movieShowList;
         }
     }
 }
