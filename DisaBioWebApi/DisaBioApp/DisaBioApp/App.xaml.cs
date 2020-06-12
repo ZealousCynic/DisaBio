5cf1e829e18b19068661657f7372ee9a535ab8ef
MDW91 <42375451+MDW91@users.noreply.github.com>
2020-06-12 13:22:14 +0200

    Gui Created for UserSettingsPage

--- DisaBioWebApi/DisaBioApp/DisaBioApp/App.xaml.cs
+++ DisaBioWebApi/DisaBioApp/DisaBioApp/App.xaml.cs
@@ -4,6 +4,7 @@ using Xamarin.Forms.Xaml;
 using DisaBioApp.Services;
 using DisaBioApp.Views;
 using DisaBioApp.Dtos;
+using DisaBioModel.Model;
 
 namespace DisaBioApp
 {
@@ -15,9 +16,10 @@ namespace DisaBioApp
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            DependencyService.Register<CinemaDataStore>();        
+            DependencyService.Register<CinemaDataStore>();
             //MainPage = new MainPage();
             //MainPage = new AboutPage();
+            //MainPage = new UserSettingsPage();
             MainPage = new CinemaPage();
         }
 
