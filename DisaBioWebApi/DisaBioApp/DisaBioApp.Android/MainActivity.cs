58afb645659e55bd2ca3afac9743ce03e55c1311
xipan <Xi Pan>
2020-06-14 15:38:59 +0200

    Create push notification

--- DisaBioWebApi/DisaBioApp/DisaBioApp.Android/MainActivity.cs
+++ DisaBioWebApi/DisaBioApp/DisaBioApp.Android/MainActivity.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
+using Android.Gms.Common;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Util;
 using PCLAppConfig;
 
 
 namespace DisaBioApp.Droid
 {
-    [Activity(Label = "DisaBioApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "DisaBioApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, LaunchMode = LaunchMode.SingleTop)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -26,7 +29,60 @@ namespace DisaBioApp.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             ConfigurationManager.Initialise(PCLAppConfig.FileSystemStream.PortableStream.Current);
             LoadApplication(new App());
+
+            if (IsPlayServiceAvailable() == false)
+            {
+                throw new Exception("This device does not have Google Play Services and cannot receive push notifications.");
+            }
+
+            CreateNotificationChannel();
         }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            if (intent.Extras != null)
+            {
+                var message = intent.GetStringExtra("message");
+                //(App.Current.MainPage as MainPage)?.AddMessage(message);
+            }
+
+            base.OnNewIntent(intent);
+        }
+
+        bool IsPlayServiceAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Debug(AppConstants.DebugTag, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Debug(AppConstants.DebugTag, "This device is not supported");
+                }
+                return false;
+            }
+            return true;
+        }
+
+        void CreateNotificationChannel()
+        {
+            // Notification channels are new as of "Oreo".
+            // There is no need to create a notification channel on older versions of Android.
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            {
+                var channelName = AppConstants.NotificationChannelName;
+                var channelDescription = String.Empty;
+                var channel = new NotificationChannel(channelName, channelName, NotificationImportance.Default)
+                {
+                    Description = channelDescription
+                };
+
+                var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+                notificationManager.CreateNotificationChannel(channel);
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
---
+++ DisaBioWebApi/DisaBioApp/DisaBioApp/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DisaBioApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnButtonClicked(object sender, EventArgs args)
+        {
+            string token = Application.Current.Properties["fcm_token"].ToString();
+            DisaBioApp.AppConstants.SubscriptionTags = new string[] { "1111111101", "11111111102" };
+
+            string regID = Xamarin.Forms.Application.Current.Properties["fcm_regID"].ToString();
+            string templateRegID = Xamarin.Forms.Application.Current.Properties["fcm_templateRegID"].ToString();
+
+            //Xamarin.Forms.Forms.Context
+            //MainActivity.InstanceCount;
+            //Android.App.Application.Context
+            //DependencyService.Get<IFirebaseService>().UpdateTags(token);
+            tokenDisplay.Text = token;
+            regIDDisplay.Text = regID;
+            templateRegIDDisplay.Text = templateRegID;
+            //fcm_templateRegID
+        }
+
+        public void AddMessage(string message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                if (messageDisplay.Children.OfType<Label>().Where(c => c.Text == message).Any())
+                {
+                    // Do nothing, an identical message already exists
+                }
+                else
+                {
+                    Label label = new Label()
+                    {
+                        Text = message,
+                        HorizontalOptions = LayoutOptions.CenterAndExpand,
+                        VerticalOptions = LayoutOptions.Start
+                    };
+                    messageDisplay.Children.Add(label);
+                }
+            });
+        }
+    }
+}
